576348f3cb297d1f78e107c606255d4ffed030dd
Niroxapex1 <taherinima28@gmail.com>
2024-07-11 14:26:21 +0330

    third update

--- changepos.py
+++ changepos.py
@@ -10,10 +10,11 @@ outer_list = []
 
 directory_path = "C:\\Users\\Nirox\\Desktop\\Projects"
 file_list = os.listdir(directory_path)
-
+#if there was any .py file remove them from directory
 for outer in file_list:
     if outer.find('.py') != -1:
         file_list.remove(outer)
+#pick all existing files in the directory of another directory
 for i in file_list:
     innerfile = os.listdir(i)
     for j in innerfile:
