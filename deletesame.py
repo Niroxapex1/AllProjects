7f1a4e7556cf8d0893a77b7577c1d25ee665ac1b
Niroxapex1 <taherinima28@gmail.com>
2024-07-11 14:22:47 +0330

    second update

--- deletesame.py
+++ deletesame.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import stat
+#if somthing wrong happend when removing this def will return
 def remove_readonly(func, path):
     # Change the file to writable and then reattempt the removal
     os.chmod(path, stat.S_IWRITE)
@@ -11,9 +12,12 @@ outer_list = []
 directory_path = "C:\\Users\\Nirox\\Desktop\\Projects"
 file_list = os.listdir(directory_path)
 
+
+#remove None folders
 for outer in file_list:
     if outer.find('.py') != -1:
         file_list.remove(outer)
+#remove all folders in previous folder
 for i in file_list:
     innerfile = os.listdir('C:\\Users\\Nirox\\Desktop\\Projects\\'+i)
     for j in innerfile:
